64405443459c483db10f19617752e606637fe591
admin <-->
2022-06-26 15:41:55 +0000

    add delays

--- jupyterlab/py38-script.py
+++ jupyterlab/py38-script.py
@@ -8,3 +8,16 @@ import sys
 print ("The script has the name %s" % (sys.argv[0]))
 arguments = len(sys.argv) - 1
 print ("The script is called with %i arguments" % (arguments))
+
+import time
+print ("Update 1 of 5...");
+time.sleep(5)
+print ("Update 2 of 5...");
+time.sleep(5)
+print ("Update 3 of 5...");
+time.sleep(5)
+print ("Update 4 of 5...");
+time.sleep(5)
+print ("Update 5 of 5...");
+
+print ("Done, thanks for waiting!");
